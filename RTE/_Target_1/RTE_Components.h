d5cc1398e27b6e3095b10c087dd237d22b3bd67c
yanMichellod <39124209+yanMichellod@users.noreply.github.com>
2018-10-04 11:42:30 +0200

    new + display

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -17,9 +17,9 @@
 #define CMSIS_device_header "stm32f7xx.h"
 
 #define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
-          #define RTE_Compiler_IO_STDIN_BKPT      /* Compiler I/O: STDIN Breakpoint */
+          #define RTE_Compiler_IO_STDIN_ITM       /* Compiler I/O: STDIN ITM */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
-          #define RTE_Compiler_IO_STDOUT_BKPT     /* Compiler I/O: STDOUT Breakpoint */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
 #define RTE_DEVICE_FRAMEWORK_CLASSIC
 #define RTE_DEVICE_HAL_COMMON
 #define RTE_DEVICE_HAL_CORTEX
--- main.s
+++ main.s
@@ -19,6 +19,10 @@
 next    EQU	0
 name	EQU	4
 age		EQU 12
+	
+storedAddress RN 4		;stock address in r4
+previousAddress RN 5	;previous address for the linked list
+emptyRegister RN 6
 
 asm_main PROC
 	push	{lr}		; save return address
@@ -26,6 +30,7 @@ asm_main PROC
 	ldr	r0,=Heap		; get start of heap
 	add	r1,r0,#4		; add 4 for next free bloock
 	str	r1,[r0]			; store in start of heap
+	mov emptyRegister , #0
 ;------------------------ ASK CHOICE ------------------------------
 loop
 	ldr	r0,=MsgAsk
@@ -62,7 +67,37 @@ loop
 ;=================================================================
 NewEntry
 ; TODO complete your code here
-
+	ldr	r0,=MsgName
+	bl	strOut					; display message
+	
+	mov r0, #16					;ask for 14 bytes to store data
+	bl	New 					;define the address to store data, in r0, for strIn
+	mov storedAddress, r0		;store the memory address
+	
+	str emptyRegister, [storedAddress] ;set the "next" 4 bytes to 0
+	
+	add r0, #name				;store the address to write the name, after the "next", for the strIn
+	mov r1, #8					;store the max length for strIn 
+	bl strIn					;call the stdIn function
+	
+	ldr	r0,=MsgAge			
+	bl	strOut					;display message
+	bl decIn					;call the strIn function
+	str r0,[storedAddress,#age]
+	
+	ldr r0, =Header 			;store the value pointed by header into r0 
+	mov previousAddress, r0
+	cmp r0, emptyRegister
+	
+While	
+	beq Next 				;if "next" pointer == 0 -> end of the list, set next 
+	mov previousAddress, r0 ;store the previous address
+	ldr r0, [r0]			;get the next address
+	cmp r0, emptyRegister	;update flags r0 - 0
+	b While					;loop
+Next 	
+	str storedAddress, [previousAddress]	;update the last pointer of the list
+	
 	b	loop
 
 
@@ -71,7 +106,29 @@ NewEntry
 ;=================================================================
 ViewDatabase
 ; TODO complete your code here
-
+	ldr r0, =Header 			;store the value pointed by header into r0 
+	ldr r0, [r0]     			;store the value pointed by header into r0
+	cmp r0, emptyRegister		; test if r0 is NULL
+	
+WhileView
+	beq NextView		; end loop if address is NULL
+	mov storedAddress,r0 ; save actual address
+	
+	ldr	r0,=MsgDspName
+	bl	strOut					; display message
+	mov r0, storedAddress
+	add r0,#name
+	bl	strOut					; display message
+	ldr	r0,=MsgDspAge
+	bl	strOut					; display message
+	ldr r0,[storedAddress,#age] ; load the address of the name
+	bl	decOut		
+	
+	mov r0,storedAddress 
+	ldr r0, [r0]	        	;get the next pointer
+	cmp r0, emptyRegister		; test if r0 is NULL
+	b WhileView
+NextView
 	B	loop			; yes -> return
 
 
